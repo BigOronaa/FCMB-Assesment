11b6646f04a75412bb1cccd6bbe14077f2e66e84
Igogo Great Oro-Oghene <edwardoronaa@gmail.com>
2026-07-07 21:55:16 +0100

    Update Terraform

---
+++ terraform/modules/ecr/main.tf
@@ -0,0 +1,20 @@
+resource "aws_ecr_repository" "this" {
+
+  name = var.repository_name
+
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  encryption_configuration {
+    encryption_type = "AES256"
+  }
+
+  tags = {
+    Project     = "FCMB"
+    ManagedBy   = "Terraform"
+    Environment = "Assessment"
+  }
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,17 @@
+variable "aws_region" {
+  description = "AWS Region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "cluster_name" {
+  description = "EKS Cluster Name"
+  type        = string
+  default     = "fcmb-eks"
+}
+
+variable "repository_name" {
+  description = "ECR Repository Name"
+  type        = string
+  default     = "fcmb-microservice"
+}
